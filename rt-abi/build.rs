ebcb6debcc0f5cb98926b40326dfbbbef8e0ad9c
Daniel Bittman <danielbittman1@gmail.com>
2025-04-21 21:19:26 +0000

    Fix llvm build.

--- rt-abi/build.rs
+++ rt-abi/build.rs
@@ -1,13 +1,31 @@
 fn main() {
+    let headers = std::env::var("TWIZZLER_ABI_BUILTIN_HEADERS").ok();
+    let sysroots = std::env::var("TWIZZLER_ABI_SYSROOTS").ok();
+    let target = std::env::var("TARGET").unwrap();
+
     let prefix = "../include/twizzler/rt";
     let mut bg = std::process::Command::new("bindgen");
+    if let Some(val) = std::env::var("TWIZZLER_ABI_LLVM_CONFIG").ok() {
+        bg.env("LLVM_CONFIG_PATH", val);
+    }
     bg.arg("--override-abi").arg(".*=C-unwind");
     bg.arg("--use-core");
     bg.arg(format!("{}/__all.h", prefix));
     bg.arg("-o")
         .arg(format!("src/bindings.rs"))
         .arg("--")
+        .arg("-target")
+        .arg(&target)
         .arg("-nostdlibinc");
+
+    if let Some(headers) = headers {
+        bg.arg("-I").arg(headers);
+    }
+
+    if let Some(sysroots) = sysroots {
+        let sysheaders = format!("{}/{}", sysroots, target);
+        bg.arg("-I").arg(sysheaders);
+    }
     let status = bg.status().expect("failed to generate bindings");
     if !status.success() {
         panic!("failed to generate bindings");
